471c5a23b454db62cc3cabf168f33e6386cc5d7c
Devon <devon.stettler@tutanota.com>
2024-08-05 22:23:27 -0400

    Added simple view

---
+++ MediaOrganizer/Views/CurrentlySelectedItemControl.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MediaOrganizer.Views;
+
+public partial class CurrentlySelectedItemControl : UserControl
+{
+    public CurrentlySelectedItemControl()
+    {
+        InitializeComponent();
+    }
+}
